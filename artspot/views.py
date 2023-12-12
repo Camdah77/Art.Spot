90d615336bc81adf6641bbcf315217e3e2f766dc
Camilla Dahlström <camilla.dahlstromz@gmail.com>
2023-12-12 09:39:00 +0000

    New folder: templates/artspot/artworks New files: artworks.html & add_artwork.html

--- artspot/views.py
+++ artspot/views.py
@@ -2,9 +2,9 @@ from django.shortcuts import render
 from .models import Artwork  # Use a different name for the model class, e.g., Artwork
 
 def get_artwork(request):
-    try:
-        artwork_instance = Artwork.objects.get(id=1)
-    except Artwork.DoesNotExist:
-        artwork_instance = None
+    artworks = Artwork.objects.all()
+    context = {
+        'artworks': artworks  # Fix the variable name here
+    }
 
-    return render(request, 'artspot/index.html', {'artwork': artwork_instance})
+    return render(request, 'artspot/artworks/artworks.html', context)
