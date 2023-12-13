079dfe1325e21f68abdb28d9e37c5b1eb87d99cc
Camilla Dahlström <camilla.dahlstromz@gmail.com>
2023-12-13 09:39:41 +0000

    server error with elephant sql: message in elephant sql: problem communicate with event stream. I take a break.

--- pp4/settings.py
+++ pp4/settings.py
@@ -31,7 +31,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 DEBUG = True
 
 ALLOWED_HOSTS = ['8000-camdah77-artspot-p6z040aiprb.ws-eu106.gitpod.io',
-                    'git.heroku.com',
+                'https://camdah77-artspot-p6z040aiprb.ws-eu106.gitpod.io/'
+                'git.heroku.com',
                 'artspot-1537d6fa1517.herokuapp.com',
                 'localhost']
 
