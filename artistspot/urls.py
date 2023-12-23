fb4f5f6feeecc42c654fdea56d359820f1a0f6c7
Camilla Dahlström <camilla.dahlstromz@gmail.com>
2023-12-23 09:18:05 +0000

    create new useruser, now: modify all models so they work as before thursday- when somthing went wrong

--- artistspot/urls.py
+++ artistspot/urls.py
@@ -1,17 +1,16 @@
+from django.contrib import admin
 from django.urls import path, include
-from artspot import views
 from artspot.views import (
-    landing_page, add_artwork, edit_artwork, delete_artwork, PostList, PostDetail, custom_login, 
-    home, about, blog, events, market, UserRegisterView, UserEditView, PasswordsChangeView, 
-    ShowProfilePageView, EditProfilePageView, CreateProfilePageView, password_success, Post_like, product, category)
+    landing_page, add_artwork, edit_artwork, delete_artwork, PostList, custom_login, LogoutView, PostDetail, custom_logout, signout,
+    home, blog, events, market, UserRegisterView, about, Post_like)
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.auth import views as auth_views
 
 app_name = 'artspot'
+
 urlpatterns = [
-    path("accounts/", include("django.contrib.auth.urls")),
-    path('admin/', admin.site.urls),
+     path('admin/', admin.site.urls),
     path('add/', add_artwork, name='add_artwork'),
     path('edit/<artwork_id>/', edit_artwork, name='edit'),
     path('delete/<artwork_id>/', delete_artwork, name='delete'),
@@ -20,21 +19,16 @@ urlpatterns = [
     path('market/', market, name='market'),
     path('blog/', PostList.as_view(), name='blog'),
     path('like/<slug:slug>/', Post_like.as_view(), name='post_like'),
-    path('members/', include('allauth.urls')),
     path('login/', custom_login, name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='members/logout.html'), name='logout'),
     path('signout/', signout, name='signout'),
-    path('logout', views.custom_logout, name='logout'),
+    path('logout', custom_logout, name='logout'),
     path('register/', UserRegisterView.as_view(), name='register'),
-    path('edit_profile/', UserEditView.as_view(), name='edit_profile'),
-    path('password/', PasswordsChangeView.as_view(template_name='members/password_change.html'), name='password_change'),
-    path('password_success', password_success, name='password_success'),
-    path('<int:pk>/profile/', ShowProfilePageView.as_view(), name='show_profile_page'),
-    path('<int:pk>/edit_profile_page/', EditProfilePageView.as_view(), name='edit_profile_page'),
-    path('product/<int:pk>/', product, name='product'),
-    path('category/<str:foo>', category, name='category'),
-    path('create_profile_page/', CreateProfilePageView.as_view(), name='create_profile_page'),
-    path('<slug:slug>/', PostDetail.as_view(), name='post_detail'),
+       path('<slug:slug>/', PostDetail.as_view(), name='post_detail'),
     path('', home, name='home'),
     path("members/", include("allauth.urls")),
 ]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- artspot/views.py
+++ artspot/views.py
@@ -1,10 +1,18 @@
+from django.http import HttpResponseRedirect
+from django.contrib.auth import login, authenticate, logout
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserChangeForm, PasswordChangeForm, UserCreationForm, AuthenticationForm
+from django.contrib.auth.views import PasswordChangeView, LogoutView
+from django.contrib import messages
 from django.shortcuts import render, redirect, get_object_or_404
-from .models import Artwork  
-from .models import Artwork, Post  , Comment
-from .forms import AddArtworkForm
-from django.http import HttpResponse
+from django.http import HttpResponseRedirect
 from django.template import loader
-from django.views import generic
+from django.urls import reverse, reverse_lazy
+from django.views import generic, View
+from django.views.generic import ListView, DetailView, CreateView, TemplateView
+from .models import Artwork, Post, Comment
+from .forms import LoginForm, CustomUserCreationForm, AddArtworkForm, CommentForm
+from django.contrib.auth.forms import UserCreationForm
 
 # HTML- pages
 def landing_page(request):
@@ -19,19 +27,125 @@ def market(request):
     return render(request, 'artworks/artworks.html')   
 def about(request):
     return render(request, 'about/aboutartspot.html')  
-def login(request):
-    return render(request, '/account/login.html')  
-def logout(request):
-    return render(request, '/account/logout.html')  
-def signup(request):
-    return render(request, 'account/signup.html')  
-# List uploaded Artworks
-def get_artwork(request):
-    artworks = Artwork.objects.all()
-    context = {
-        'artworks': artworks  
-    }
-    return render(request, 'artworks/artworks.html', context)
+def custom_login(request):
+    return render(request, 'members/login.html')
+def signout(request):
+    return render(request, 'members/logout.html')
+def UserRegisterView(request):
+    return render(request, 'members/register.html')  
+def profile(request):
+    return render(request, 'members/profile.html')  
+def signout(request):
+    return render(request, 'members/logout.html')
+
+
+
+
+
+# members/login.html
+def custom_login(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, "Welcome. We wish you a creative day!")
+            return redirect('home')
+        else:
+            messages.success(request, "Please try again")
+            return redirect('members/login.html')
+    
+    else:
+        return render(request, 'members/login.html', {})
+
+# members/register.html
+class UserRegisterView(generic.CreateView):
+    form_class = UserCreationForm
+    template_name = 'members/register.html'
+    success_url = reverse_lazy('login')
+def custom_logout(request):
+    logout(request)
+    messages.info(request, "Logged out successfully!")
+    return redirect("home")
+
+
+# members/logout.html
+def LogoutView(request):
+     return render(request, 'members/logout.html')
+     return render(request, 'members/login.html')
+
+
+# artmarket/products
+def product(request, pk):
+    product = Product.objects.get(id=pk)
+    return render(request, 'artworks/product.html', {'product': product})
+def category(request, foo):
+	# Replace Hyphens with Spaces
+	foo = foo.replace('-', ' ')
+	# Grab the category from the url
+	try:
+		# Look Up The Category
+		category = Category.objects.get(name=foo)
+		products = Product.objects.filter(category=category)
+		return render(request, 'artworks/category.html', {'products':products, 'category':category})
+	except:
+		messages.success(request, ("That Category Doesn't Exist..."))
+		return redirect('home')
+  
+class PostDetail(View):
+    def get(self, request, slug, *args, **kwargs):
+        print(f"Slug value: {slug}")  # Add this line
+        queryset = Post.objects.filter(status=1)
+        post = get_object_or_404(queryset, slug=slug)
+        comments = post.comments.filter(approved=True).order_by("-created_on")
+        liked = False
+        if post.likes.filter(id=self.request.user.id).exists():
+            liked = True
+        return render(
+            request,
+            "blogg/post_detail.html",
+            {
+                "post": post,
+                "comments": comments,
+                "commented": False,
+                "liked": liked,
+                "comment_form": CommentForm()
+            },
+        )
+    def post(self, request, slug, *args, **kwargs):
+        queryset = Post.objects.filter(status=1)
+        post = get_object_or_404(queryset, slug=slug)
+        comments = post.comments.filter(approved=True).order_by("-created_on")
+        liked = False
+        if post.likes.filter(id=self.request.user.id).exists():
+            liked = True
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            comment_form.instance.email = request.user.email
+            comment_form.instance.name = request.user.username
+            comment = comment_form.save(commit=False)
+            comment.post = post
+            comment.save()
+        else:
+            comment_form = CommentForm()
+        return render(
+            request,
+            "blogg/post_detail.html",
+            {
+                "post": post,
+                "comments": comments,
+                "commented": True,
+                "comment_form": comment_form,
+                "liked": liked
+            },
+        )
+
+    
+# Artmarket 
+def market(request):
+    products = Product.objects.all()
+    return render(request, 'artworks/artworks.html', {'products':products})
 # Add artworks
 def add_artwork(request):
     if request.method == 'POST':
@@ -63,12 +177,44 @@ def delete_artwork(request, artwork_id):
     artwork = get_object_or_404(Artwork, id=artwork_id)
     artwork.delete()
     return redirect('get_artwork')
-    return redirect('get_artwork')
-
-
 #BLOG
 class PostList(generic.ListView):
     model = Post
     queryset = Post.objects.filter(status=1).order_by("-created_on")
-    template_name = "/blogg/blog.html"
+    template_name = "blogg/blog.html"
     paginate_by = 6
+    def post(self, request, slug, *args, **kwargs):
+        queryset = Post.objects.filter(status=1)
+        post = get_object_or_404(queryset, slug=slug)
+        comments = post.comments.filter(approved=True).order_by("-created_on")
+        liked = False
+        if post.likes.filter(id=self.request.user.id).exists():
+            liked = True
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            comment_form.instance.email = request.user.email
+            comment_form.instance.name = request.user.username
+            comment = comment_form.save(commit=False)
+            comment.post = post
+            comment.save()
+        else:
+            comment_form = CommentForm()
+        return render(
+            request,
+            "blogg/post_detail.html",
+            {
+                "post": post,
+                "comments": comments,
+                "commented": True,
+                "comment_form": comment_form,
+                "liked": liked
+            },
+        )
+class Post_like(View):
+    def post(self, request, slug, *args, **kwargs):
+        post = get_object_or_404(Post, slug=slug)
+        if post.likes.filter(id=request.user.id).exists():
+            post.likes.remove(request.user)
+        else:
+            post.likes.add(request.user)
+        return HttpResponseRedirect(reverse('artspot:post_detail', args=[slug]))
