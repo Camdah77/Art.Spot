d7033fa0fbd07d670dfde94a99cc947637c7cf0a
Camilla Dahlström <camilla.dahlstromz@gmail.com>
2023-12-23 07:10:27 +0000

    search for broken link in repositry

--- artspot/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class ArtspotConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'artspot'
--- artspot/tests_models.py
+++
@@ -1,8 +0,0 @@
-from django.test import TestCase
-from .models import Artwork
-
-class TestModels(TestCase):
-
-    def test_done_defaults_to_false(self):
-        artwork = Artwork.objects.create(name='Test Artwork Model')
-        self.assertFalse(artwork.done)
